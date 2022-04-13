511643ba2022cd5253dec39e0b90414f3ce019db
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2022-04-13 12:13:11 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# poc-firebase-products
-PoC Google Firebase Products CRUD
+## Description
+PoC Firebase CRUD products
+
+## Deploy Firebase Project
+
+Create a Firebase project from [Firebase Console](https://console.firebase.google.com/)
+
+![Firebase Console](captures/firebase_console.png "Firebase Console")
+
+STEP 01:
+
+![Firebase project name](captures/firebase_project_name.png "Firebase project name")
+
+STEP 02:
+
+Select if we want active Google Analytics.
+
+![Firebase Google Analytics](captures/firebase_google_analytics.png "Firebase Google Analytics")
+
+STEP 03:
+
+![Firebase Project Confirmation](captures/firebase_project_confirmation.png "Firebase Project Confirmation")
+
+## Configure Firebase Project
+
+Now we create a Firestore Database a NoSQL database from Google
+
+![Firebase Firestore Database](captures/firebase_firestore_creation.png "Firebase Firestore Database")
+
+Firestore from firebase console
+
+![Firestore Database](captures/firestore_database.png "Firestore 
+Database")
+
+Firestore from Google Clooud Platform
+
+![GCP Firestore Database](captures/gcp_firestore_database.png "GCP Firestore 
+Database")
+
+## Degub
+
+```sh
+firebase serve
+```
+
+## Deploy
+
+If we obtaine errirs from eslint remove the dot point from package lint scripts
+```sh
+firebase deploy
+```
+
+![Firebase Deployment](captures/firebase_deploy.png "Firebase Deployment")
+
+Obtaine the firebase uri
+
+```sh
+https://us-central1-product-3a5fb.cloudfunctions.net/app
+```
+
+Firebase from Google Functions on Google Cloud Platform
+![Google Functions GCP](captures/firebase_gcp.png "Google Functions GCP")
---
+++ functions/index.js
@@ -0,0 +1,121 @@
+const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+var permisos = require("./permissions.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(permisos),
+  databaseURL: "https://product-3a5fb.firebaseio.com"
+});
+
+const db = admin.firestore();
+
+app.use(cors({ origin: true }));
+
+function output(code, input){
+    var today = new Date();
+    var date = today.getFullYear()+'-'
+    +(today.getMonth()+1)+'-'
+    +today.getDate()+"|"
+    +today.getHours() + ":" 
+    + today.getMinutes() + ":" 
+    + today.getSeconds();
+    
+    if(code === "200") return {
+        message : "Process completed successfully",
+        date : date,
+        result : input
+    }
+
+    if(code === "201") return {
+        message : "Entity created successfully",
+        date : date,
+        result : input
+    }
+
+    if(code === "500") return {
+        message: "Internal server error",
+        date : date,
+        result : input
+    }
+
+    return {
+        message: "Internal server error",
+        date : date,
+        result : input
+    }
+}
+
+app.post('/api/products', (req, res) => {
+    (async () => {
+        try {
+          await db.collection('products').doc('/' + req.body.id + '/')
+              .create({
+                  code: req.body.code,
+                  description: req.body.description,
+                  price: req.body.price,
+                  active: req.body.active
+              });
+            
+          return res.status(200).send(output("200", "Product created successfully"));
+        } catch (error) {
+          console.log(error);
+          return res.status(500).send(output("500", error));
+        }
+    })();
+});
+
+app.get("/api/products", async (req, res) => {
+  try{
+      
+      let query = db.collection("products");
+      
+      const querySnapshot = await query.get();
+      let docs = querySnapshot.docs;
+
+      const response = docs.map((doc) => ({
+          id: doc.id,
+          code: doc.data().code,
+          description: doc.data().description,
+          price: doc.data().price,
+          active: doc.data().active
+      }));
+
+      return res.status(200).json(output("200", response));
+  } catch (error) {
+      return res.status(500).json(output("500", error));
+  }
+})
+
+app.put("/api/products/:id", async (req, res) => {
+  try {      
+      const document = db.collection("products").doc(req.params.id);
+      
+      await document.update({
+          code: req.body.code,
+          description: req.body.description,
+          price: req.body.price,
+          active: req.body.active,
+      });
+      
+      return res.status(200).json(output("200", "Product updated successfully"));
+  } catch (error) {
+    return res.status(500).json(output("500", error));
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  try {
+      const doc = db.collection("products").doc(req.params.id);
+      
+    await doc.delete();
+    return res.status(200).json(output("200", "Product deleted successfully"));
+  } catch (error) {
+    return res.status(500).send(output("500", error));
+  }
+});
+
+exports.app = functions.https.onRequest(app);
